7e0914b91b6e54f8daa63b14e53cdf631bbde53b
Emmanuel Adoum <adoumouangnamouemmanuel@gmail.com>
2026-05-04 18:55:23 +0000

    chore: drizzle config pg: password update

--- packages/db/drizzle.config.pg.ts
+++ packages/db/drizzle.config.pg.ts
@@ -7,6 +7,6 @@ export default defineConfig({
   dbCredentials: {
     url:
       process.env.DATABASE_URL ??
-      "postgresql://edutrack:edutrack@127.0.0.1:5433/edutrack",
+      "postgresql://edutrack:EDU90062033track@127.0.0.1:5433/edutrack",
   },
 });
